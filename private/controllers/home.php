2f3046d0d1c5cb92c1d98b5f940cf81171b96105
Jovana Nikolic <jovana.nikolic137@gmail.com>
2022-08-10 11:49:01 +0200

    Finished login/singup

--- private/controllers/home.php
+++ private/controllers/home.php
@@ -1,8 +1,21 @@
 <?php
 class Home extends Controller{
 
-    function index()
-    {
+    function index(){
+
+        if (!Auth::is_logged_in()) {
+            $this->redirect('login');
+        }
+
+        
+        $user = new User();
         echo $this->view('home');
-    }
+
+        // $user->insert($data);
+        // $user->update($id, $data);
+        // $user->delete($id);
+
+
+
+    }   
 }
